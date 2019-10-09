17dcfb9af89db68845b3af00a37e11614b867835
angelavanderklugt <56338270+angelavanderklugt@users.noreply.github.com>
2019-10-10 09:07:25 +1030

    Add files via upload

---
+++ main-1-1.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <iostream>
+#include "Savanna.h"
+using namespace std;
+int main(){
+int choice;
+cout<<"Enter a number to choose terrain:"<<endl;
+cout<<"1 = Savanna, 2 = Mountain, 3 = Jungle"<<endl;
+cin>>choice;
+
+switch (choice){
+case(1):
+Savanna* one=new Savanna("?");
+int rare=one->get_rarity();
+cout<<rare<<endl;
+cout<<one->get_animal(rare)<<endl;
+cout<<one->get_ID()<<endl;
+delete one;
+return 0;
+break;
+
+//case(2):
+//Mountain* one=new Mountain("?");
+//break;
+
+
+//case(3):
+//Jungle* one=new Jungle("?");
+//break;
+}
+}
